7033c7965701c0fef77ec270bc03b9772f83aeb7
Jason Hsu <jhsu802701@jasonhsu.com>
2022-07-27 23:23:46 -0500

    test-upload.sh includes all steps

--- test-upload.sh
+++ test-upload.sh
@@ -11,16 +11,14 @@ PATHNAME=~/.ssh/$FILENAME
 
 if [ ! -f "$PATHNAME" ]
 then
-  echo 'STEP 1: generating SSH key'
-
   # Use default path name for SSH key files.
   # Piping in the newline character means automatically pressing enter.
+  echo 'STEP 1: generating SSH key'
   ssh-keygen -t "$TYPE" -N '' -C 'jhsu802701@shell.sf.net' <<<$'\n'
-  # ssh-keygen -t "$TYPE" -f "$PATHNAME" -N '' -C 'jhsu802701@shell.sf.net' -q
-  # echo 'ssh-keyscan'
-  # ssh-keyscan -H frs.sf.net >> ~/.ssh/known_hosts
-  # echo 'ssh-copy-id'
-  # ssh-copy-id -i "$PATHNAME" 'jhsu802701@shell.sourceforge.net'
+
+  # No password needed to log in.
+  echo 'STEP 2: ssh-keyscan'  
+  ssh-keyscan -H frs.sourceforge.net >> ~/.ssh/known_hosts
 else
   echo "Skipping SSH key generation (already present at $PATHNAME)"
 fi
@@ -29,13 +27,13 @@ mkdir -p tmp1
 TIME_STAMP=`date -u +%Y%m%d-%H%M%S`
 echo "$TIME_STAMP" > tmp1/timestamp.txt
 
-echo 'STEP 2: Go to https://sourceforge.net/auth/shell_services .'
+echo 'STEP 3: Go to https://sourceforge.net/auth/shell_services .'
 echo "Copy the contents of $PATHNAME.pub to SSH Public Keys and"
 echo 'click on "Save".'
 echo 'Press Enter when finished.'
 read
 
-echo 'STEP 3: uploading tmp1/timestamp.txt'
+echo 'STEP 4: uploading tmp1/timestamp.txt'
 rsync -avPz -e ssh "tmp1/timestamp.txt" jhsu802701@frs.sourceforge.net:/home/frs/p/swiftlinux/test-upload-manual/$MX_VERSION/
 
 # The second iteration of this script should be fully automatic.
