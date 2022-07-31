85e476828d1a18e92ed2a46d1336a84dbc64bbf2
Jason Hsu <jhsu802701@jasonhsu.com>
2022-07-31 17:43:21 -0500

    Added bin/mount-compressed-fs (#15)

--- definitions.sh
+++ definitions.sh
@@ -27,3 +27,12 @@ then
 fi
 
 TIME_STAMP=`cat tmp/timestamp.txt`
+
+FILE_SQUASHFS_ORIG=''
+
+if [ -f $DIR_MAIN/tmp/path_squashfs.txt ]
+then
+  FILE_SQUASHFS_ORIG=`cat $DIR_MAIN/tmp/path_squashfs.txt`
+fi
+
+DIR_SQUASHFS_ORIG=$DIR_REMASTER/squash-orig
