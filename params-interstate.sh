901f90b9b0ce167797925831baf8269840e60af8
Jason Hsu <jhsu802701@jasonhsu.com>
2022-08-08 15:31:24 -0500

    Added params scripts for Hannah Montana, Interstate, and Twilight Zone editions

---
+++ params-interstate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# NOTE: set -o pipefail is needed to ensure that any error or failure causes the whole pipeline to fail.
+# Without this specification, the CI status will provide a false sense of security by showing builds
+# as succeeding in spite of errors or failures.
+set -eo pipefail
+
+DIR_MAIN=$PWD
+
+EDITION_SHORT_NAME='interstate'
+EDITION_LONG_NAME='Interstate Swift Linux'
+
+if [ ! -f $DIR_MAIN/tmp/edition-short-name.txt ]
+then
+  echo "$EDITION_SHORT_NAME" > $DIR_MAIN/tmp/edition-short-name.txt
+fi
+
+if [ ! -f $DIR_MAIN/tmp/edition-long-name.txt ]
+then
+  echo "$EDITION_LONG_NAME" > $DIR_MAIN/tmp/edition-long-name.txt
+fi
