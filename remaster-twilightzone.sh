9860d4106d36211eef77f29b005b405989441758
Jason Hsu <jhsu802701@jasonhsu.com>
2022-08-12 21:47:21 -0500

    Added remaster-*.sh scripts for Hannah Montana, Interstate, and Twilight Zone Linux

---
+++ remaster-twilightzone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# NOTE: set -o pipefail is needed to ensure that any error or failure causes the whole pipeline to fail.
+# Without this specification, the CI status will provide a false sense of security by showing builds
+# as succeeding in spite of errors or failures.
+set -eo pipefail
+
+PARAM1='twilightzone'
+PARAM2='Twilight Zone Swift Linux'
+
+bash set-params.sh "$PARAM1" "$PARAM2"
+
+mkdir -p log
+TIME_STAMP_LOCAL=`date -u +%Y-%m%d-%H%M%S`
+LOG_FILE="log/$PARAM1-$TIME_STAMP_LOCAL.txt"
+bash remaster-local.sh 2>&1 | tee "$LOG_FILE"
