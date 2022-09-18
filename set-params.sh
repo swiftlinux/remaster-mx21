9f6e847b56a60f410eedd7846e594885b3359dc6
Jason Hsu <jhsu802701@jasonhsu.com>
2022-09-17 23:27:32 -0500

    Removed bin/replace_sif; replaced with bin/string-in-file script (#58)

--- set-params.sh
+++ set-params.sh
@@ -12,5 +12,5 @@ mkdir -p tmp-edition
 echo "EDITION_SHORT='EDITION_SHORT_1'" > tmp-edition/definitions.sh
 echo "EDITION_LONG='EDITION_LONG_1'" >> tmp-edition/definitions.sh
 
-bin/replace_sif 'EDITION_SHORT_1' "$EDITION_SHORT_LOCAL" "tmp-edition/definitions.sh"
-bin/replace_sif 'EDITION_LONG_1' "$EDITION_LONG_LOCAL" "tmp-edition/definitions.sh"
+bin/string-in-file 'replace' 'tmp-edition/definitions.sh' 'EDITION_SHORT_1' "$EDITION_SHORT_LOCAL"
+bin/string-in-file 'replace' 'tmp-edition/definitions.sh' 'EDITION_LONG_1' "$EDITION_LONG_LOCAL"
